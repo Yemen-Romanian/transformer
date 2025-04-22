1dab7f6d029b82e8c65c929792c0d05ebc0e3152
Yevhen <romanyak@yevhen@gmail.com>
2025-04-22 21:50:59 +0000

    Fixes to positional encoding

--- model/positional_encoding.py
+++ model/positional_encoding.py
@@ -1,16 +1,17 @@
 import torch.nn as nn
 import torch
 import math
+import matplotlib.pyplot as plt
 
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, max_len=5000, dropout=0.1):
         super(PositionalEncoding, self).__init__()
         assert d_model > 0
 
-        pos_indices = torch.arange(0, max_len)
+        pos_indices = torch.arange(0, max_len).unsqueeze(1)
         embedding_indices = torch.arange(0, d_model, 2)
 
-        self.pe = torch.zeros((max_len, d_model))
+        pe = torch.zeros((max_len, d_model))
         div_term = torch.exp(embedding_indices * -(math.log(10000.0) / d_model))
         pe[:, 0::2] = torch.sin(pos_indices * div_term)
         pe[:, 1::2] = torch.cos(pos_indices * div_term)
